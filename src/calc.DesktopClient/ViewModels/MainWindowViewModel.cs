957c1992fd7b0d99e47a3f890de10390baa75400
wdefeder <rafael.roznowicz@gmail.com>
2020-03-30 17:06:52 +0200

    Add compiled application assembly (single exe containing all needed .Net dependencies)

--- src/calc.DesktopClient/ViewModels/MainWindowViewModel.cs
+++ src/calc.DesktopClient/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace calc.DesktopClient.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Calcualtor ";
         public string Title
         {
             get { return _title; }
